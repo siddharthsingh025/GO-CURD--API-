d04b58f51d5c55723c4148b54a55b46e660327a7
SIDDHARTH <87073574+siddharthsingh025@users.noreply.github.com>
2023-02-26 10:54:49 +0530

    CURD API completed- all work fine

--- controllers/postController.go
+++ controllers/postController.go
@@ -11,12 +11,20 @@ func PostsCreate(c *gin.Context) {
 
 	//Get data off req body
 
+	var body struct {
+		Title string
+		Body  string
+	}
+
+	//here we bind our req data to body
+	c.Bind(&body)
+
 	//Create a post
-	post := models.POST{Title: "Jinzhu", Body: "post body"}
+	post := models.POST{Title: body.Title, Body: body.Body}
 
 	result := initializer.DB.Create(&post)
 
-	if result != nil {
+	if result.Error != nil {
 		c.Status(400)
 		return
 	}
@@ -26,4 +34,69 @@ func PostsCreate(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"posts": post,
 	})
+}
+
+func PostIndex(c *gin.Context) {
+	//Get the posts
+
+	// Get all records
+	var post []models.POST
+	initializer.DB.Find(&post)
+
+	//respon d to them
+	c.JSON(200, gin.H{
+		"posts": post,
+	})
+}
+
+func PostShow(c *gin.Context) {
+	//Get id from URL
+	id := c.Param("id")
+
+	//Get the post
+
+	var post models.POST
+	initializer.DB.First(&post, id)
+	// SELECT * FROM users WHERE id = 10;
+
+	//respon d to them
+	c.JSON(200, gin.H{
+		"posts": post,
+	})
+}
+
+func PostUpdate(c *gin.Context) {
+	//Get the id of URL
+	id := c.Param("id")
+
+	//Get the data off req body from context
+	var body struct {
+		Title string
+		Body  string
+	}
+
+	//here we bind our req data to body
+	c.Bind(&body)
+
+	//Find the post by id
+	var post models.POST
+	initializer.DB.First(&post, id)
+	//updating
+
+	initializer.DB.Model(&post).Updates(models.POST{Title: body.Title, Body: body.Body})
+
+	//responed back
+	c.JSON(200, gin.H{
+		"posts": post,
+	})
+}
+
+func PostsDelete(c *gin.Context) {
+	// GEt the id of url
+	id := c.Param("id")
+	//delete the particular post
+	initializer.DB.Delete(&models.POST{}, id)
+
+	//respond back
+	c.Status(200)
 }
--- main.go
+++ main.go
@@ -16,5 +16,9 @@ func main() {
 
 	r := gin.Default()
 	r.POST("/posts", controllers.PostsCreate)
+	r.GET("/posts", controllers.PostIndex)
+	r.GET("/posts/:id", controllers.PostShow)
+	r.PUT("/posts/:id", controllers.PostUpdate)
+	r.DELETE("/posts/:id", controllers.PostsDelete)
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
